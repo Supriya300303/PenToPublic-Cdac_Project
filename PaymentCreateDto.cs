9cb0d0abefc35407d1cd8226108ba63118063a22
Supriya300303 <supriyakulkarni3003@gmail.com>
2025-08-06 23:39:16 +0530

    Add files via upload

---
+++ PaymentCreateDto.cs
@@ -0,0 +1,7 @@
+﻿public class PaymentCreateDto
+{
+    public int UserId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string PaymentMode { get; set; }
+}
